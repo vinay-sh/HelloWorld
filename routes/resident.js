1728da145df82b1dca9de86711d06a2e57e1524c
Vinay <vinayshiremath@gmail.com>
2016-12-05 18:23:02 -0800

    resident queries

--- routes/resident.js
+++ routes/resident.js
@@ -4,6 +4,7 @@ var TAG = "RESIDENT : ";
 
 exports.addNewResident = function(req, res){
     console.log(TAG + "Adding resident");
+    var result = {};
     mongo.connect(function(err, db){
     	if(err){
 			console.log(TAG + "Unable to connect to DB");
@@ -11,8 +12,28 @@ exports.addNewResident = function(req, res){
 			result.status = TAG + "Unable to connect to DB";
 			res.json(result);
 		}else{
+            var coll = mongo.collection('resident')
     	    console.log(req.body.email);
 			console.log(TAG + "Connected to DB");
+            coll.insertOne(
+                {
+                    "_id": req.body.email,
+                    "first_name": req.body.fname,
+                    "last_name": req.body.lname,
+                    "email": req.body.email
+                    "address": req.body.address
+                },function(err, docs){
+                    if(err){
+                        result.code=208;
+                        result.status="Failed to add the new resident to DB";
+                        res.json(result);
+                    }else{
+                        result.code=200;
+                        result.status="Successfulky added a new resident";
+                        res.json(result);
+                    }
+                }
+            )
 		}
     });
     res.send();
@@ -20,6 +41,7 @@ exports.addNewResident = function(req, res){
 
 exports.updateNewResident = function (req,res) {
 	console.log(TAG + "Updating user");
+    var result = {};
 	mongo.connect(function (err, db) {
 		if(err){
             console.log(TAG + "Unable to connect to DB");
@@ -34,6 +56,7 @@ exports.updateNewResident = function (req,res) {
 
 exports.getResidentReports = function(req, res) {
 	console.log(TAG + "Getting Resident Reports");
+    var result = {};
     mongo.connect(function (err, db) {
         if(err){
             console.log(TAG + "Unable to connect to DB");
@@ -41,13 +64,16 @@ exports.getResidentReports = function(req, res) {
             result.status = TAG + "Unable to connect to DB";
             res.json(result);
         }else{
+            var coll = mongo.collection('resident')
             console.log(TAG + "Connected to DB");
+
         }
     });
 };
 
 exports.fileReport = function(req, res){
     console.log(TAG + "Filing Report");
+    var result = {};
     mongo.connect(function (err, db) {
         if(err){
             console.log(TAG + "Unable to connect to DB");
@@ -55,13 +81,42 @@ exports.fileReport = function(req, res){
             result.status = TAG + "Unable to connect to DB";
             res.json(result);
         }else{
+            var coll = mongo.collection('reports')
             console.log(TAG + "Connected to DB");
+            coll.insertOne(
+                {
+                    "res_id": req.body.email,
+                    "location": {
+                        "latitude":req.body.lat,
+                        "longitude":req.body.lon
+                    },
+                    "description":req.bosy.des
+                    "size": req.body.size
+                    "severity": req.body.severity
+                    "status": req.body.status
+                    "date": req.body.date
+                    "time":req.body.time
+
+
+                },function(err, docs){
+                    if(err){
+                        result.code=208;
+                        result.status="Failed to add the new resident to DB";
+                        res.json(result);
+                    }else{
+                        result.code=200;
+                        result.status="Successfulky added a new report";
+                        res.json(result);
+                    }
+                }
+            )
         }
     });
 };
 
 exports.updateReport = function(req, res){
     console.log(TAG + "Updating Report");
+    var result = {};
     mongo.connect(function (err, db) {
         if(err){
             console.log(TAG + "Unable to connect to DB");
@@ -69,6 +124,7 @@ exports.updateReport = function(req, res){
             result.status = TAG + "Unable to connect to DB";
             res.json(result);
         }else{
+            var coll = mongo.collection('resident')
             console.log(TAG + "Connected to DB");
         }
     });
